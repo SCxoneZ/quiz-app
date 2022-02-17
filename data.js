630d479c943ef4940a452e965a7e662da0492abf
SCxoneZ <stevenniuschandraa@gmail.com>
2022-02-17 18:28:39 +0700

    creating all logic in script.js

--- data.js
+++ data.js
@@ -9,5 +9,15 @@ export default
       "d": "hyper active philosophy"
     },
     "answer": "b"
+  },
+  {
+    "question": "Who's The Creator Of Javascript?",
+    "options": {
+      "a": "James Bond",
+      "b": "Steve Jobs",
+      "c": "Brendan Eich",
+      "d": "Bill Gates"
+    },
+    "answer": "c"
   }
 ];
--- script.js
+++ script.js
@@ -19,10 +19,47 @@
 
 import datas from './data.js';
 
-const questionBox = document.querySelector('.container .question'),
-optionsBox = document.querySelector('.container .options ul'),
-button = document.querySelector('button.submit');
+const button = document.querySelector('button.submit');
+const questionBox = document.querySelector('.container .question');
+const optionsBox = document.querySelector('.container .options ul');
+let currentIndex = 0;
 
-questionBox.innerHTML = datas[0].question;
+changeQuiz(currentIndex);
 
-const newLi = document.createElement('li');
+button.addEventListener('click', () => {
+  const options = document.querySelectorAll('.container .options ul li input:checked');
+  
+  if(options.length == 1){
+    if(options[0].id == datas[currentIndex].answer){
+      alert('Correct!');
+    }
+    else{
+      alert('Incorrect!');
+  }
+  optionsBox.innerHTML = '';
+  ++currentIndex;
+  changeQuiz(currentIndex);
+}
+  
+});
+
+function changeQuiz(index) {
+  questionBox.innerHTML = datas[index].question;
+  for (const option in datas[index].options) {
+    const newLi = document.createElement('li');
+
+    const newInput = document.createElement('input');
+    newInput.setAttribute('type', 'radio');
+    newInput.setAttribute('id', option);
+    newInput.setAttribute('name', 'option');
+
+    const newLabel = document.createElement('label');
+    newLabel.setAttribute('for', option);
+    newLabel.innerHTML = datas[index].options[option];
+
+    newLi.appendChild(newInput);
+    newLi.appendChild(newLabel);
+
+    optionsBox.appendChild(newLi);
+  }
+}
